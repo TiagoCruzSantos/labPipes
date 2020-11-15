15c646d2b805c1dfd5efb88217155f3cab1b43bc
Tiago da Cruz Santos <tiago.unity.cruz@gmail.com>
2020-11-15 04:37:04 -0300

    pipe

--- main.c
+++ main.c
@@ -7,25 +7,38 @@ int main(){
     scanf("%d", &x);
     if(x < 1 || x > 5){
         fprintf(stderr, "X fora da faixa\n");
+        return 2;
+    }
+    int pipeP0P1[2];
+    int pipeP1P2[2];
+    if(pipe(pipeP0P1) < 0){
+        perror("Não foi possível abrir o pipe");
+        return 1;
+    }
+    if(pipe(pipeP1P2) < 0){
+        perror("Não foi possivel abrir o pipe");
+        close(pipeP0P1[0]);
+        close(pipeP0P1[1]);
         return 1;
     }
     pid_t P1 = fork();
     if(P1 == -1){
         perror("Fork falhou");
-        return 1;
+        return 3;
     }
     if(P1 == 0){
 
     }else{
+        int pipeP0P2[2];
         pid_t P2 = fork();
         if(P2 == -1){
             perror("Fork falhou");
-            return 1;
+            return 3;
         }
         if(P2 == 0){
 
         }else{
-            
+
         }
     }
 }
